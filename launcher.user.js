7e2488e8813102c66ea852d66444bf25673324c8
Dachi122 <dachi0102@gmail.com>
2015-06-28 01:41:12 -0700

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -973,7 +973,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  names = ["NotReallyABot"],
+  names = [""],
   originalName = names[Math.floor(Math.random() * names.length)],
   sessionScore = 0,
   serverIP = "",
